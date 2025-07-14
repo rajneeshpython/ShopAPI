ad951b59eaf2bb584367dc7f96bd02f16a104610
Rajneesh Kumar <RajneeshPondy@users.noreply.github.com>
2025-07-14 08:17:02 +0530

    Add user registration API with role support for vendor and customer

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_vendor = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=True)  # default role
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password', 'is_vendor', 'is_customer']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password'],
+            is_vendor=validated_data.get('is_vendor', False),
+            is_customer=validated_data.get('is_customer', True),
+        )
+        return user
